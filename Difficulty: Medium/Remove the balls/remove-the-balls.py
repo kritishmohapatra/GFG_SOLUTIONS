d1c88b92c7f3ca7b42d851edf345308da535dd9a
Kritish Mohapatra <kritishmohapatra06norisk@gmail.com>
2025-06-11 00:10:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove the balls/remove-the-balls.py
@@ -0,0 +1,12 @@
+#User function Template for python3
+class Solution:
+    def findLength(self, color, radius):
+        n=len(color)
+        st=[]
+        for i in range(n):
+            if st and color[i]==color[st[-1]] and radius[i]==radius[st[-1]]:
+                st.pop()
+            else:
+                st.append(i)
+        return len(st)
+            
