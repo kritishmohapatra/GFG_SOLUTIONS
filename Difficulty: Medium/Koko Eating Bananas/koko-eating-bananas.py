f21131a89ad2e3479c261780ef8f997c865cd662
Kritish Mohapatra <kritishmohapatra06norisk@gmail.com>
2025-06-13 19:17:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+
+class Solution:
+    def kokoEat(self,arr,k):
+        # Code here
+        def calci(arr, k):
+            total=0
+            for i in arr:
+                total+=math.ceil(i/k)
+            return total
+        
+        low=1
+        high=max(arr)
+        while(low<=high):
+            mid=(low+high)//2
+            total=calci(arr, mid)
+            if total<=k:
+                high=mid-1
+            else:
+                low=mid+1
+        return low
