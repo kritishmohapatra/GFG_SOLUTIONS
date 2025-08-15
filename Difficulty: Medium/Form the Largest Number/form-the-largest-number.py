e3eefabc54d6c548f6773a4858d6332a00cf74a9
Kritish Mohapatra <kritishmohapatra06norisk@gmail.com>
2025-08-16 00:39:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Form the Largest Number/form-the-largest-number.py
@@ -0,0 +1,15 @@
+from functools import cmp_to_key
+class Solution:
+    def mycmp(self, s1, s2):
+        if s1+s2>s2+s1:
+            return -1
+        else:
+            return 1
+            
+	def findLargest(self, arr):
+	    # code here
+	    nums=[str(ele) for ele in arr]
+	    nums.sort(key=cmp_to_key(self.mycmp))
+	    if nums[0]=="0":
+	        return "0"
+	    return "".join(nums)
