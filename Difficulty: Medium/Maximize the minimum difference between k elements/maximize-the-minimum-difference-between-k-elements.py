1daa4af24758c8f651cf69afc4e9764df7dc97a6
Kritish Mohapatra <kritishmohapatra06norisk@gmail.com>
2025-08-21 00:14:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximize the minimum difference between k elements/maximize-the-minimum-difference-between-k-elements.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isPossible(self, arr, k, mid):
+        count = 1
+        last = arr[0]
+    
+        for i in range(1, len(arr)):
+            if arr[i] - last >= mid:
+                count += 1
+                last = arr[i]
+            if count == k:
+                return True
+    
+        return False
+    def maxMinDiff(self, arr, k):
+        # code here
+        arr.sort()
+
+        # define binary search range
+        low = 0
+        high = arr[-1] - arr[0]
+        answer = 0
+    
+        # binary search to find max valid min-diff
+        while low <= high:
+            mid = (low + high) // 2
+    
+            if self.isPossible(arr, k, mid):
+                answer = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+    
+        # return the maximum feasible 
+        # minimum difference
+        return answer
+            
