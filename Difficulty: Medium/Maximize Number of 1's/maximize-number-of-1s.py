8116c48f7fb4926c7ecf95162984869e08387865
Kritish Mohapatra <kritishmohapatra06norisk@gmail.com>
2025-08-28 00:22:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximize Number of 1's/maximize-number-of-1s.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxOnes(self, arr, k):
+        # code here
+        res=0
+        start=0
+        end=0
+        cnt=0
+        while end<len(arr):
+            if arr[end]==0:
+                cnt+=1
+            while cnt>k:
+                if arr[start]==0:
+                    cnt-=1
+                start+=1
+            res=max(res, (end-start)+1)
+            end+=1
+        return res
+        
