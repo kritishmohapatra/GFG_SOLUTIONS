f11fc7481d2929b5215bee261fe264c7c104a195
Kritish Mohapatra <kritishmohapatra06norisk@gmail.com>
2025-09-06 13:54:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Middle of a Linked List/middle-of-a-linked-list.py
+++ Difficulty: Easy/Middle of a Linked List/middle-of-a-linked-list.py
@@ -1,16 +1,13 @@
-# your task is to complete this function
-
 '''
 class node:
     def __init__(data):
         self.data = data
         self.next = None
 '''
+
 class Solution:
-    #  Should return data of middle node. If linked list is empty, then  -1
     def getMiddle(self, head):
-        # Code here
-        # return the value stored in the middle node
+        # code here
         slow=head
         fast=head
         while fast!=None and fast.next!=None:
