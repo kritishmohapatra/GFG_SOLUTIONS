7b808caf4ccc0562d4451ed625859a2e9e02a327
Kritish Mohapatra <kritishmohapatra06norisk@gmail.com>
2025-12-24 01:44:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Asteroid Collision/asteroid-collision.py
@@ -0,0 +1,18 @@
+#User function Template for python3
+
+class Solution:
+    def asteroidCollision(self, N, asteroids):
+        # Code here
+        n=len(asteroids)
+        st=[]
+        for i in range(n):
+            if asteroids[i]>0:
+                st.append(asteroids[i])
+            else:
+                while (st and st[-1]>0 and st[-1]<abs(asteroids[i])):
+                    st.pop()
+                if st and st[-1]==abs(asteroids[i]):
+                    st.pop()
+                elif not st or st[-1]<0:
+                    st.append(asteroids[i])
+        return st
