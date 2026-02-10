dfca41b776c9e1bab9cf76abf77ed04e0fb7716e
Kritish Mohapatra <kritishmohapatra06norisk@gmail.com>
2026-02-11 00:41:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Equalize the Towers/equalize-the-towers.py
+++ Difficulty: Medium/Equalize the Towers/equalize-the-towers.py
@@ -1,20 +1,21 @@
 class Solution:
     def minCost(self, heights, cost):
         # code here
-        def compute_cost(h):
-            r = 0
+        def getCost(h):
+            total = 0
             for i in range(len(heights)):
-                r += cost[i] * abs(heights[i] - h)
-            return r
-            
-        lo, hi = min(heights), max(heights)
-        while hi - lo > 2:
-            m1 = lo + (hi-lo)//3
-            m2 = hi - (hi-lo)//3
-            c1, c2 = compute_cost(m1), compute_cost(m2)
-            if c1 > c2:
-                lo = m1
+                total += abs(heights[i] - h) * cost[i]
+            return total
+
+        left = min(heights)
+        right = max(heights)
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if getCost(mid) <= getCost(mid + 1):
+                right = mid
             else:
-                hi = m2
-        
-        return min(compute_cost(h) for h in range(lo, hi + 1))
+                left = mid + 1
+
+        return getCost(left)
