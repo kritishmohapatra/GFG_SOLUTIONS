bf42b3d3e965e34c522c192e02a761fb123a0d50
Kritish Mohapatra <kritishmohapatra06norisk@gmail.com>
2026-04-09 18:52:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Intersection of Two Sorted Arrays/intersection-of-two-sorted-arrays.py
@@ -0,0 +1,16 @@
+class Solution:
+    def intersection(self,a, b):
+        # code here
+        ans=[]
+        i, j=0, 0
+        while i<len(a) and j<len(b):
+            if a[i]<b[j]:
+                i+=1
+            elif a[i]>b[j]:
+                j+=1
+            else:
+                if not ans or ans[-1]!=a[i]:
+                    ans.append(a[i])
+                i+=1
+                j+=1
+        return ans
