4da3414ca8ba4a752d11c0079b43001d952065b8
Kritish Mohapatra <kritishmohapatra06norisk@gmail.com>
2026-06-16 18:14:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Remove Spaces/remove-spaces.py
+++ Difficulty: Basic/Remove Spaces/remove-spaces.py
@@ -1,8 +1,5 @@
 class Solution:
     def removeSpaces(self, s):
         # code here
-        st=""
-        for i in s:
-            if i!=" ":
-                st+=i 
-        return st
+        o=s.replace(" ", "")
+        return o
